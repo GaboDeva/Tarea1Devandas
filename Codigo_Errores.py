69b933d0edeaf6f38e43b77ec94865c964c1ae7c
GaboDeva <80008525+GaboDeva@users.noreply.github.com>
2021-03-05 01:42:59 -0600

    Codigo con errores detectables por Flake8

---
+++ Codigo_Errores.py
@@ -0,0 +1,20 @@
+import time
+
+b = 12 #variable a utilizar
+   
+def y_rectax(x, x, b):
+
+    #toma los valores introducidos para obtener el valor y de una rexta "mx+b"
+
+    y = (m * x) + b
+    return y
+
+# solicita los valores deseados
+
+m = int(input("Introduzca el valor de m de la recta: "))
+x = int(input("introduzca el valor de x de la recta: "))
+
+print("El valor de y es: ", y_rectax(x, m, c))
+    
+
+    
